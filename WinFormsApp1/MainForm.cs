f2a42d09341c94371ecffb8d6c7c076ef63c929b
323 <323@323-2>
2025-12-02 12:37:43 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp1/MainForm.cs
@@ -0,0 +1,60 @@
+using static WinFormsApp1.UsersClasses.InfoEmailSending;
+using System.Net;
+using System.Windows.Forms;
+using WinFormsApp1.UsersClasses;
+
+namespace WinFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            textBoxEmail.Text = "task_code_development@list.ru";
+            textBoxName.Text = "Антон Игоревич";
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            // Проверка TextBox на наличие значений
+            if (String.IsNullOrWhiteSpace(textBoxEmail.Text) ||
+                String.IsNullOrWhiteSpace(textBoxName.Text) ||
+                String.IsNullOrWhiteSpace(textBoxSubject.Text) ||
+                String.IsNullOrWhiteSpace(textBoxBody.Text))
+            {
+                MessageBox.Show("Заполните все поля!");
+                return;
+            }
+            //Ввод данных с формы в объекты ранее созданных классов
+            string smtp = "smtp.mail.ru";
+            //Необходимо ввести свой mail.ru адрес!!! И мвоё ФИО
+            StringPair fromInfo = new StringPair("official.works@inbox.ru, Бахмаева Милана Руслановна");
+            //Необходимо ввести свой пароль который вывел mail.ru !!!
+            string password = "MzPsavulsS2d7GKHZ5As";
+
+            StringPair toInfo = new StringPair(textBoxEmail.Text, textBoxName.Text);
+            string subject = textBoxSubject.Text;
+            string body = $"{DateTime.Now}\n" +
+            $"{Dns.GetHostName()}\n" +
+            $"{Dns.GetHostEntry(Dns.GetHostName()).AddressList.First()}\n" +
+            $"{textBoxBody.Text}";
+
+            InfoEmailSending info =
+                new InfoEmailSending(smtp, fromInfo, password, toInfo, subject, body);
+
+            // Отправка данных в виде электронного письма
+            SendingEmail sendingEmail = new SendingEmail(info);
+            sendingEmail.Send();
+
+            // Уведомление для пользователя и очистка всех TextBox
+            MessageBox.Show("Письмо отправлено!");
+            foreach (TextBox textBox in Controls.OfType<TextBox>())
+                textBox.Text = "";
+        }
+    }
+}
